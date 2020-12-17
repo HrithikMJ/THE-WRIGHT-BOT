d0e580dedc7a3637ad1c34bd11b636a9da340616
HritikMj <hrithikmj00@gmail.com>
2020-12-17 09:25:03 +0530

    2nd

--- wright.py
+++ wright.py
@@ -1,43 +1,46 @@
-import speech_recognition as sr 
-import pyttsx3  
-  
+import speech_recognition as sr
+import pyttsx3
+
 
 r = sr.Recognizer()
 j=0
 
-def SpeakText(command): 
-      
-   
-    engine = pyttsx3.init() 
-    engine.say(command)  
-    engine.runAndWait() 
-    
-      
-while(1):     
-      
-    
-    try: 
-          
-        
-        with sr.Microphone() as source2: 
-              
-              
-            r.adjust_for_ambient_noise(source2, duration=0.2) 
-              
-             
-            audio2 = r.listen(source2) 
-              
-             
-            MyText = r.recognize_google(audio2) 
-            MyText = MyText.lower() 
+def SpeakText(command):
+
+
+    engine = pyttsx3.init()
+    engine.say(command)
+    engine.runAndWait()
+
+
+while(1):
+
+
+    try:
+
+
+        with sr.Microphone() as source2:
+
+
+            r.adjust_for_ambient_noise(source2, duration=0.2)
+
+
+            audio2 = r.listen(source2)
+
+
+            MyText = r.recognize_google(audio2)
+            MyText = MyText.lower()
             print(MyText)
-            if mytext=="wright" or mytext=="right":\
+            if Mytext=="wright" or Mytext=="right":
                 j+=1
+                print(j)
+            elif MyText=="exit":
+                breaK
 
-                 
 
-    except sr.RequestError as e: 
-        print("Could not request results; {0}".format(e)) 
-          
-    except sr.UnknownValueError: 
-        print("unknown error occured")    
+
+    except sr.RequestError as e:
+        print("Could not request results; {0}".format(e))
+
+    except sr.UnknownValueError:
+        print("unknown error occured")
